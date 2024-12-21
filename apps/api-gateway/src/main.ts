f0f1d4ee42875432c8fe0f90ebd48688ffca264e
Ruben Merson Nzaou <rm.nzaou.etudes@gmail.com>
2024-12-21 02:02:57 +0100

    chore: initial commit

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -12,7 +12,9 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+  console.log('hello test');
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
