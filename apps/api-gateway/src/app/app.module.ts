df3fda1ef58c979dc01f91be6cd9b9d4b4fb0ed2
Ruben Merson Nzaou <rm.nzaou.etudes@gmail.com>
2024-12-24 14:16:14 +0100

    feat: add configs to load env vars

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { validateEnvironment } from '../config/env.validator';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: validateEnvironment, // Validate environment variables
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
