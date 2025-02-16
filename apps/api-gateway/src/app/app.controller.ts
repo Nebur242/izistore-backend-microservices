45fda77ef3d7305ff821aebf8788f61867fa9cfa
Ruben Merson Nzaou <rm.nzaou.etudes@gmail.com>
2025-02-16 21:44:06 +0100

    chore: setup ci/cd

--- apps/api-gateway/src/app/app.controller.ts
+++ apps/api-gateway/src/app/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AdminOnly } from '../auth/decorators/admin-only.decorator';
 import { AuthUser, Claims } from '../auth/decorators/get-user.decorator';
 
 @Controller('/app')
@@ -8,7 +7,6 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @AdminOnly()
   getData(@Claims() user: AuthUser) {
     console.log(user);
     return this.appService.getData();
